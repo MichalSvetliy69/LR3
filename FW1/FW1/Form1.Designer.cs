b235b0df7822fa995cf85e42b856fcd8eafe8ef3
MichalSvetliy <105512312+MichalSvetliy69@users.noreply.github.com>
2022-10-28 00:37:44 +0700

    Changes (10/28/2022)

--- FW1/FW1/Form1.Designer.cs
+++ FW1/FW1/Form1.Designer.cs
@@ -30,59 +30,276 @@
         {
             this.tbName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.LoX = new System.Windows.Forms.Button();
+            this.btnHello = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.создатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.правкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.видToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.справкаToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolPanel = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.Brush4 = new System.Windows.Forms.Button();
+            this.Brush3 = new System.Windows.Forms.Button();
+            this.brush1 = new System.Windows.Forms.Button();
+            this.Brush2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip2.SuspendLayout();
+            this.toolPanel.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // tbName
             // 
-            this.tbName.Location = new System.Drawing.Point(164, 49);
+            this.tbName.Location = new System.Drawing.Point(135, 426);
+            this.tbName.Margin = new System.Windows.Forms.Padding(2);
             this.tbName.Name = "tbName";
-            this.tbName.Size = new System.Drawing.Size(174, 22);
+            this.tbName.Size = new System.Drawing.Size(119, 20);
             this.tbName.TabIndex = 1;
+            this.tbName.TextChanged += new System.EventHandler(this.tbName_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(31, 49);
+            this.label1.Location = new System.Drawing.Point(132, 181);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(127, 16);
+            this.label1.Size = new System.Drawing.Size(102, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Введите свое имя:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // LoX
+            // btnHello
             // 
-            this.LoX.Location = new System.Drawing.Point(22, 102);
-            this.LoX.Name = "LoX";
-            this.LoX.Size = new System.Drawing.Size(316, 35);
-            this.LoX.TabIndex = 3;
-            this.LoX.Text = "Hello";
-            this.LoX.UseVisualStyleBackColor = true;
-            this.LoX.Click += new System.EventHandler(this.LoX_Click);
+            this.btnHello.Location = new System.Drawing.Point(34, 328);
+            this.btnHello.Margin = new System.Windows.Forms.Padding(2);
+            this.btnHello.Name = "btnHello";
+            this.btnHello.Size = new System.Drawing.Size(237, 28);
+            this.btnHello.TabIndex = 3;
+            this.btnHello.Text = "Hello";
+            this.btnHello.UseVisualStyleBackColor = true;
+            this.btnHello.Click += new System.EventHandler(this.btnHello_Click);
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(22, 162);
+            this.btnExit.Location = new System.Drawing.Point(67, 249);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(316, 33);
+            this.btnExit.Size = new System.Drawing.Size(237, 27);
             this.btnExit.TabIndex = 4;
             this.btnExit.Text = "Выйти";
             this.btnExit.UseVisualStyleBackColor = true;
-            this.btnExit.Click += new System.EventHandler(this.button2_Click);
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.справкаToolStripMenuItem,
+            this.правкаToolStripMenuItem,
+            this.видToolStripMenuItem,
+            this.справкаToolStripMenuItem1});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(1264, 24);
+            this.menuStrip2.TabIndex = 6;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.создатьToolStripMenuItem,
+            this.сохранитьToolStripMenuItem,
+            this.открытьToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.справкаToolStripMenuItem.Text = "Файл";
+            this.справкаToolStripMenuItem.Click += new System.EventHandler(this.справкаToolStripMenuItem_Click);
+            // 
+            // создатьToolStripMenuItem
+            // 
+            this.создатьToolStripMenuItem.Name = "создатьToolStripMenuItem";
+            this.создатьToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.создатьToolStripMenuItem.Text = "Создать";
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            // 
+            // правкаToolStripMenuItem
+            // 
+            this.правкаToolStripMenuItem.Name = "правкаToolStripMenuItem";
+            this.правкаToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.правкаToolStripMenuItem.Text = "Правка";
+            // 
+            // видToolStripMenuItem
+            // 
+            this.видToolStripMenuItem.Name = "видToolStripMenuItem";
+            this.видToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.видToolStripMenuItem.Text = "Вид";
+            // 
+            // справкаToolStripMenuItem1
+            // 
+            this.справкаToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.оПрограммеToolStripMenuItem});
+            this.справкаToolStripMenuItem1.Name = "справкаToolStripMenuItem1";
+            this.справкаToolStripMenuItem1.Size = new System.Drawing.Size(65, 20);
+            this.справкаToolStripMenuItem1.Text = "Справка";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            // 
+            // toolPanel
+            // 
+            this.toolPanel.Controls.Add(this.groupBox1);
+            this.toolPanel.Controls.Add(this.tbName);
+            this.toolPanel.Controls.Add(this.label1);
+            this.toolPanel.Controls.Add(this.btnHello);
+            this.toolPanel.Controls.Add(this.btnExit);
+            this.toolPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.toolPanel.Location = new System.Drawing.Point(0, 24);
+            this.toolPanel.Name = "toolPanel";
+            this.toolPanel.Size = new System.Drawing.Size(339, 601);
+            this.toolPanel.TabIndex = 10;
+            this.toolPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Brush4);
+            this.groupBox1.Controls.Add(this.Brush3);
+            this.groupBox1.Controls.Add(this.trackBar1);
+            this.groupBox1.Controls.Add(this.brush1);
+            this.groupBox1.Controls.Add(this.Brush2);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(339, 150);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Выбор кисти";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.AutoSize = false;
+            this.trackBar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.trackBar1.Location = new System.Drawing.Point(3, 16);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(333, 45);
+            this.trackBar1.TabIndex = 0;
+            // 
+            // Brush4
+            // 
+            this.Brush4.BackgroundImage = global::FW1.Properties.Resources.Снимок3;
+            this.Brush4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Brush4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Brush4.Location = new System.Drawing.Point(150, 67);
+            this.Brush4.Name = "Brush4";
+            this.Brush4.Size = new System.Drawing.Size(50, 50);
+            this.Brush4.TabIndex = 4;
+            this.Brush4.UseVisualStyleBackColor = true;
+            // 
+            // Brush3
+            // 
+            this.Brush3.BackgroundImage = global::FW1.Properties.Resources.Снимок2;
+            this.Brush3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Brush3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Brush3.Location = new System.Drawing.Point(219, 67);
+            this.Brush3.Name = "Brush3";
+            this.Brush3.Size = new System.Drawing.Size(52, 50);
+            this.Brush3.TabIndex = 3;
+            this.Brush3.UseVisualStyleBackColor = true;
+            // 
+            // brush1
+            // 
+            this.brush1.BackgroundImage = global::FW1.Properties.Resources.Снимок1;
+            this.brush1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.brush1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.brush1.Location = new System.Drawing.Point(12, 67);
+            this.brush1.Name = "brush1";
+            this.brush1.Size = new System.Drawing.Size(52, 50);
+            this.brush1.TabIndex = 1;
+            this.brush1.UseVisualStyleBackColor = true;
+            this.brush1.Click += new System.EventHandler(this.brush1_Click);
+            // 
+            // Brush2
+            // 
+            this.Brush2.BackgroundImage = global::FW1.Properties.Resources.Снимок11;
+            this.Brush2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Brush2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Brush2.Location = new System.Drawing.Point(83, 67);
+            this.Brush2.Name = "Brush2";
+            this.Brush2.Size = new System.Drawing.Size(50, 50);
+            this.Brush2.TabIndex = 2;
+            this.Brush2.UseVisualStyleBackColor = true;
+            this.Brush2.Click += new System.EventHandler(this.Brush2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(343, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(921, 585);
+            this.panel1.TabIndex = 11;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(921, 585);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(381, 233);
-            this.Controls.Add(this.btnExit);
-            this.Controls.Add(this.LoX);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.tbName);
+            this.ClientSize = new System.Drawing.Size(1264, 625);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.toolPanel);
+            this.Controls.Add(this.menuStrip2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "tbText";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.Click += new System.EventHandler(this.Form1_Click);
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.toolPanel.ResumeLayout(false);
+            this.toolPanel.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -92,8 +309,27 @@
 
         private System.Windows.Forms.TextBox tbName;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button LoX;
+        private System.Windows.Forms.Button btnHello;
         private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem создатьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem правкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem видToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.Panel toolPanel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button Brush4;
+        private System.Windows.Forms.Button Brush3;
+        private System.Windows.Forms.Button Brush2;
+        private System.Windows.Forms.Button brush1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- FW1/FW1/Form1.cs
+++ FW1/FW1/Form1.cs
@@ -8,13 +8,42 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
 namespace FW1
 {
+  
     public partial class Form1 : Form
     {
+
+
+        void CreateBlank(int width, int height)
+        {
+            var OldImage = pictureBox1.Image;
+            var bmp = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    bmp.SetPixel(i, j, DefaultColor);
+                }
+            }
+            pictureBox1.Image = bmp;
+            if (OldImage != null)
+            {
+                OldImage.Dispose();
+            }
+
+        }
+        Color DefaultColor
+        {
+            get { return Color.White; }
+        }
         public Form1()
         {
             InitializeComponent();
+
+            CreateBlank(pictureBox1.Width, pictureBox1.Height);
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -22,9 +51,9 @@ namespace FW1
 
         }
 
-        private void LoX_Click(object sender, EventArgs e)
-        {/*
-            string UserName = tbName.Name.Text.Trim();
+        private void btnHello_Click(object sender, EventArgs e)
+        {
+            string UserName = tbName.Text.Trim();
             if (UserName.Length < 3)
             {
                 MessageBox.Show("Введите имя пользователя:");
@@ -32,10 +61,10 @@ namespace FW1
             else
             {
                 MessageBox.Show("Привет " + UserName);
-            }*/
+            }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void btnExit_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
@@ -43,6 +72,81 @@ namespace FW1
         private void Form1_Click(object sender, EventArgs e)
         {
            
+        }
+
+        private void tbName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void справкаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripComboBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripSeparator1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripProgressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void toolStrip2_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Brush2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void brush1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
